1fd0feb214df1aef7a5c74c5e515b5f0f2bd2cc3
Salin Dhakal <salindhakal7@gmail.com>
2025-08-23 21:44:51 +0545

    homepage additions

---
+++ frontend/src/components/ChatContainer.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+import { useEffect } from "react";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
+const ChatContainer = () => {
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser.id);
+  }, [selectedUser.id, getMessages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
+};
+
+export default ChatContainer;
